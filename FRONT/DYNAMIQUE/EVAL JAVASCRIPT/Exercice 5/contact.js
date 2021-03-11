7bba8f0c3533e252b69ab2fd74f6fd6bb10f9a8e
80010-48-01 <80010-48-01@exchange.ad.afpanet>
2021-03-11 10:34:10 +0100

    Exercice 5 terminé EVAL JS terminée

--- FRONT/DYNAMIQUE/EVAL JAVASCRIPT/Exercice 5/contact.js
+++ FRONT/DYNAMIQUE/EVAL JAVASCRIPT/Exercice 5/contact.js
@@ -1,21 +1,24 @@
 
 // DECLARATION DES VARIABLES
 
-var yourName = document.getElementById("yourName"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP NAME
+var yourName = document.getElementById("yourName"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP NAME
 var nameReg = "^[a-zA-Zéèàêëäï\\-\\ ]{1,50}$"; // REGEX POUR VERIFICATION DE NAME, SURNAME ET TOWN
-var surname = document.getElementById("surname");  // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP SURNAME
-var sexf = document.getElementById("sexf");  // RECOIT LA VALEUR DE L UTILISATEUR POUR LE CHAMP SEXF
-var sexm = document.getElementById("sexm");  // RECOIT LA VALEUR DE L UTILISATEUR POUR LE CHAMP SEXM
-var postalcode = document.getElementById("postalcode"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP POSTALCODE
+var surname = document.getElementById("surname");  // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP SURNAME
+var sexf = document.getElementById("sexf");  // RECOIT LA VALEUR DE L UTILISATEUR DU CHAMP SEXF
+var sexm = document.getElementById("sexm");  // RECOIT LA VALEUR DE L UTILISATEUR DU CHAMP SEXM
+var dateOfBirth = document.getElementById("dateOfBirth");  // RECOIT LA VALEUR DE L UTILISATEUR DU CHAMP DATEOFBIRTH
+var diff; // SERT A LA VERIFICATION DU CHAMP DATEOFBIRTH
+var majeur; // // SERT A LA VERIFICATION DU CHAMP DATEOFBIRTH
+var postalcode = document.getElementById("postalcode"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP POSTALCODE
 var postalcodeReg = "^[0-9]{5}$"; // REGEX POUR VERIFICATION DU CODE POSTAL
-var address = document.getElementById("address"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP ADDRESS
+var address = document.getElementById("address"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP ADDRESS
 var addressReg = "^[0-9]*[a-zA-Z0-9éèàêëäï\\-\\ ]{1,50}$"; // REGEX POUR VERIFICATION DE L ADRESSE
-var town = document.getElementById("town"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP TOWN
-var email = document.getElementById("email"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP EMAIL
+var town = document.getElementById("town"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP TOWN
+var email = document.getElementById("email"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP EMAIL
 var emailReg = "^[a-z0-9._-]+@[a-z0-9._-]{2,}\.[a-z]{2,4}$"; // REGEX POUR VERIFICATION DE L EMAIL
-var select = document.getElementById("subject-select"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE SELECT
-var question = document.getElementById("question"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR POUR LE CHAMP QUESTION
-var accept = document.getElementById("accept"); // RECOIT LA VALEUR DE L UTILISATEUR POUR LE CHAMP ACCEPT
+var select = document.getElementById("subject-select"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU SELECT
+var question = document.getElementById("question"); // RECOIT LA VALEUR SAISIE PAR L UTILISATEUR DU CHAMP QUESTION
+var accept = document.getElementById("accept"); // RECOIT LA VALEUR DE L UTILISATEUR DU CHAMP ACCEPT
 var error;
 var yourNameError;
 var surnameError;
@@ -27,39 +30,55 @@ var selectError;
 var questionError;
 var acceptError;
 
-
 //  DEBUT FONCTION DE VERIFICATION DU FORMULAIRE
 document.getElementById("form").addEventListener("submit", function checkForm(e) {
     
     // VERIF CHAMP NAME
-    if (!yourName.value.match(nameReg)) // CONDITION DE VERIFICATION
+    if (!yourName.value.match(nameReg)) // CONTROLE DE SAISIE
     {
-        yourNameError = "*Merci de saisir votre nom !"; // CONTENU DU MESSAGE D ERREUR
+        yourNameError = "*Vous n'avez pas saisi votre nom !"; // CONTENU DU MESSAGE D ERREUR
         error = yourNameError;
         document.getElementById("your-name-error").innerHTML = yourNameError; // AFFICHAGE DU MESSAGE D ERREUR
     }
     // VERIF CHAMP NAME
 
     // VERIF CHAMP SURNAME
-    if (!surname.value.match(nameReg)) // CONDITION DE VERIFICATION
+    if (!surname.value.match(nameReg)) // CONTROLE DE SAISIE
     {
-        surnameError = "*Merci de saisir votre prénom !"; // CONTENU DU MESSAGE D ERREUR
+        surnameError = "*Vous n'avez pas saisi votre prénom !"; // CONTENU DU MESSAGE D ERREUR
         error = surnameError;
         document.getElementById("surname-error").innerHTML = surnameError; // AFFICHAGE DU MESSAGE D ERREUR
     }
     // VERIF CHAMP SURNAME
 
     //VERIF CHAMP SEX
-    if ((form.sexf.checked == false) && (form.sexm.checked == false)) // CONDITION DE VERIFICATION
+    if ((form.sexf.checked == false) && (form.sexm.checked == false)) // CONTROLE DE SAISIE
     {
-        sexError = "*Merci de cocher un bouton !"; // CONTENU DU MESSAGE D ERREUR
+        sexError = "*Merci de cocher une case !"; // CONTENU DU MESSAGE D ERREUR
         error = sexError;
         document.getElementById("sex-error").innerHTML = sexError; // AFFICHAGE DU MESSAGE D ERREUR
     }
-    //VERIF CHAMP SEX
+    // FIN VERIF CHAMP SEX
+
+    // VERIF UTILISATEUR MAJEUR
+    if (dateOfBirth.value == 0) // CONTROLE DE SAISIE
+    {
+        dateOfBirthError = "*Vous devez entrer votre date de naissance !"; // CONTENU DU MESSAGE D ERREUR
+        error = dateOfBirthError;
+        document.getElementById("date-of-birth-error").innerHTML = dateOfBirthError; // AFFICHAGE DU MESSAGE D ERREUR
+    }
+    diff = Date.now() - new Date(dateOfBirth.value).getTime();  // VERIFICATION DE L AGE DE L UTILISATEUR
+    majeur = (18*365*24*3600*1000);
+    if (diff < majeur)
+    {
+        dateOfBirthError = "*Vous devez être majeur pour envoyer ce formulaire !"; // CONTENU DU MESSAGE D ERREUR
+        error = dateOfBirthError;
+        document.getElementById("date-of-birth-error").innerHTML = dateOfBirthError; // AFFICHAGE DU MESSAGE D ERREUR
+    }
+    // FIN VERIF UTILISATEUR MAJEUR
 
     // VERIF CHAMP POSTALCODE
-    if (!postalcode.value.match(postalcodeReg)) // CONDITION DE VERIFICATION
+    if (!postalcode.value.match(postalcodeReg)) // CONTROLE DE SAISIE
     {
         postalcodeError = "*Veuillez saisir un code postal à 5 chiffres"; // CONTENU DU MESSAGE D ERREUR
         error = postalcodeError;
@@ -68,7 +87,7 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     // FIN VERIF CHAMP POSTALCODE
     
     // VERIF CHAMP ADDRESS
-    if (!postalcode.value.match(addressReg)) // CONDITION DE VERIFICATION
+    if (!postalcode.value.match(addressReg)) // CONTROLE DE SAISIE
     {
         addressError = "*Veuillez saisir une adresse"; // CONTENU DU MESSAGE D ERREUR
         error = addressError;
@@ -77,7 +96,7 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     // FIN VERIF CHAMP ADDRESS
 
     // VERIF CHAMP TOWN
-    if (!town.value.match(nameReg)) // CONDITION DE VERIFICATION
+    if (!town.value.match(nameReg)) // CONTROLE DE SAISIE
     {
         townError = "*Veuillez saisir la ville"; // CONTENU DU MESSAGE D ERREUR
         error = townError;
@@ -86,16 +105,16 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     // FIN VERIF CHAMP TOWN
 
     // VERIF CHAMP EMAIL
-    if (!email.value.match(emailReg)) // CONDITION DE VERIFICATION
+    if (!email.value.match(emailReg)) // CONTROLE DE SAISIE
     {
-        emailError = "*Veuillez un email valide"; // CONTENU DU MESSAGE D ERREUR
+        emailError = "*Veuillez entrer un email valide"; // CONTENU DU MESSAGE D ERREUR
         error = emailError;
         document.getElementById("email-error").innerHTML = emailError; // AFFICHAGE DU MESSAGE D ERREUR
     }
     // FIN VERIF CHAMP EMAIL
 
     // VERIF CHAMP SELECT 
-    if (select.value == "")  // CONDITION DE VERIFICATION
+    if (select.value == "")  // CONTROLE DE SAISIE
     {
         selectError = "*La sélection d'un sujet est requis"; // CONTENU DU MESSAGE D ERREUR
         error = selectError;
@@ -104,7 +123,7 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     // FIN VERIF CHAMP SELECT
 
     // VERIF CHAMP QUESTION
-    if (question.value == "")  // CONDITION DE VERIFICATION
+    if (question.value == "")  // CONTROLE DE SAISIE
     {
         questionError = "*Veuillez saisir votre question"; // CONTENU DU MESSAGE D ERREUR
         error = questionError;
@@ -113,17 +132,17 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     // FIN VERIF CHAMP QUESTION
 
     // VERIF CHAMP ACCEPT
-    if (form.accept.checked == false)  // CONDITION DE VERIFICATION
+    if (form.accept.checked == false)  // CONTROLE DE SAISIE
     {
         
-        acceptError = "*Vous devez cocher cette case pour envoyer le formulaire"; // CONTENU DU MESSAGE D ERREUR
+        acceptError = "*Vous devez accepter le traitement informatique pour envoyer ce formulaire"; // CONTENU DU MESSAGE D ERREUR
         error = acceptError;
         document.getElementById("accept-error").innerHTML = acceptError; // AFFICHAGE DU MESSAGE D ERREUR
     }
     // FIN VERIF CHAMP ACCEPT
     
     // BLOCAGE / VALIDATION DE L ENVOI DU FORMULAIRE
-    if (error)  // CONDITION DE VERIFICATION
+    if (error)  // CONTROLE DE SAISIE
     {
         e.preventDefault(); // BLOCAGE DU RECHARGEMENT DE LA PAGE
         
@@ -133,7 +152,6 @@ document.getElementById("form").addEventListener("submit", function checkForm(e)
     {
         alert("Formulaire envoyé"); // MESSAGE DE VALIDATION D ENVOI DU FORMULAIRE
     }
-    
     // FIN BLOCAGE / VALIDATION DE L ENVOI DU FORMULAIRE
 
 }
