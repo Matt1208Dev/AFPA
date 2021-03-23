13b30cfb55fff60426059f7556797ce13b26f67c
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2021-03-23 11:18:47 +0100

    EVAL SQL terminée

--- BACK/BDD/EVAL MySQL/exercice2_northwind.sql
+++ BACK/BDD/EVAL MySQL/exercice2_northwind.sql
@@ -31,3 +31,60 @@ ON `customers`.customerID = `orders`.customerID
 WHERE country = 'france' AND 'Nbre commandes'> '10'
 GROUP BY CompanyName
 HAVING COUNT(OrderID)>10;
+
+-- 5 LISTE DES CLIENTS AYANT UN CA > 30000 --
+
+SELECT CompanyName, SUM(UnitPrice*Quantity) AS CA, country 
+FROM `customers` 
+JOIN `orders` 
+ON `customers`.customerID = `orders`.customerID 
+JOIN `order details` 
+ON `order details`.orderID = `orders`.orderID 
+GROUP BY CompanyName, country 
+HAVING SUM(UnitPrice*Quantity)> 30000 
+ORDER BY `CA` DESC;
+
+-- 6 LISTE DES PAYS DONT LES CLIENTS ONT PASSE COMMANDE DE PRODUITS EXOTIC LIQUIDS --
+
+SELECT `customers`.country AS 'Pays'
+FROM `customers`
+JOIN `orders` 
+ON `customers`.customerID = `orders`.customerID 
+JOIN `order details` 
+ON `order details`.orderID = `orders`.orderID
+JOIN `products`
+ON `products`.productID = `order details`.productID
+JOIN `suppliers`
+ON `suppliers`.supplierID = `products`.supplierID
+WHERE `suppliers`.CompanyName = 'exotic liquids'
+GROUP BY `customers`.country;
+
+-- 7 MONTANT DES VENTES DE 1997 --
+
+SELECT SUM(UnitPrice*Quantity) AS 'Montant Ventes 97'
+FROM `order details`
+JOIN `orders`
+ON `order details`.orderID = `orders`.orderID
+WHERE OrderDate LIKE '1997%';
+
+-- 8 MONTANT DES VENTES DE 1997 MOIS PAR MOIS --
+
+SELECT MONTH(OrderDate) AS 'Mois 97', SUM(UnitPrice*Quantity) AS 'Montant Ventes'
+FROM `order details`
+JOIN `orders`
+ON `order details`.orderID = `orders`.orderID
+WHERE OrderDate LIKE '1997%'
+GROUP BY MONTH(OrderDate);
+
+-- 9 DEPUIS QUELLE DATE LE CLIENT 'DU MONDE ENTIER' N A T IL PLUS COMMANDE --
+
+SELECT max(OrderDate) AS 'Date de dernière commande'
+FROM `orders`
+JOIN `customers`
+ON `orders`.customerID = `customers`.customerID
+WHERE CompanyName = 'du monde entier';
+
+-- 10 QUEL EST LE DELAI MOYEN DE LIVRAISON EN JOURS --
+
+SELECT FLOOR(AVG(DATEDIFF(ShippedDate, OrderDate))) AS 'Délai moyen de livraison en jours'
+FROM `orders`;
