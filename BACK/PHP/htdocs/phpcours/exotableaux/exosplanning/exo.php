3ab92acc09ba65e178c2a54df8b464916a8709b3
Matthieu Gueulle <gueulle.matthieu60@gmail.com>
2021-03-30 10:43:20 +0200

    exos tableaux php terminés

--- BACK/PHP/htdocs/phpcours/exotableaux/exosplanning/exo.php
+++ BACK/PHP/htdocs/phpcours/exotableaux/exosplanning/exo.php
@@ -1,21 +1,42 @@
 <?php
 
+// ARRAY DE DEPART
 $a = array("19001" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "", "", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", "Validation"), 
      "19002" => array("Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Validation", ""), 
      "19003" => array("", "", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Centre", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "Stage", "", "", "Validation") 
     );
 
-foreach ($a as $key => $values) 
+foreach ($a as $key => $values) // AFFICHAGE DU TABLEAU
 {
     echo '<b>Groupe de stagiaires ' . $key . '</b><br><br>';
     foreach ($values as $week => $place)
     {
-        echo 'lieu de présence : ' . $place . '<br>';
+        echo 'Présence : ' . $place . '<br>';
     }
     echo '<br>';
 }
 
-$validation = array_search('19002', $a);
-echo $validation;
+// EXERCICE 1
+
+$stagiaires_19002 = $a['19002'];
+$validation = array_search('Validation', $stagiaires_19002);
+echo 'Exercice 1<br>Le groupe 19002 effectuera sa semaine de validation en semaine ' . ($validation+1) . '.<br>';
+
+// EXERCICE 2
+
+$stagiaires_19001 = $a['19001'];
+$fin_stage = max(array_keys($stagiaires_19001, 'Stage'))+1;
+echo '<br>Exercice 2<br>la dernière semaine de stage du groupe 19001 interviendra en semaine ' . $fin_stage . '.<br>';
+
+// EXERCICE 3 
+
+echo '<br>Exercice 3<br>';
+print_r(array_keys($a));
+
+// EXERCICE 4
+
+$stagiaires_19003 = $a['19003'];
+$duree_stage = count(array_keys($stagiaires_19003, 'Stage'));
+echo '<br><br>Exercice 4<br>Le stage du groupe 19003 dure ' . $duree_stage . ' semaines.';
 
 ?>
